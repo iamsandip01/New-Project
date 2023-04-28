8c5cb9298f0778710112defeee61d4b41b8196f3
iamsandip01 <115915158+iamsandip01@users.noreply.github.com>
2023-04-28 23:02:37 +0530

    Update BookController.java

--- springdemoProductManagementSystem/src/main/java/com/example/Controller/BookController.java
+++ springdemoProductManagementSystem/src/main/java/com/example/Controller/BookController.java
@@ -25,33 +25,33 @@ public class BookController {
 	private BookService bookService;
 	
 	
-	@PostMapping("/book")
+	@PostMapping(path="/book")
 	public Book addBook(@RequestBody Book book) {
 		return this.bookService.addBook(book);
 	}
 	
-	@GetMapping("/book")
+	@GetMapping(path="/book")
 	public List<Book> getAllBook(){
 		return this.bookService.getAllBook();
 	}
-	@GetMapping("/book/{id}")
+	@GetMapping(path="/book/{id}")
 	public Book getBookById(@PathVariable int id){
 		return this.bookService.getBookById(id);
 	}
-	@GetMapping("/book/{name}")
+	@GetMapping(path="/book/{name}")
 	public Book findBookByName(@PathVariable String name) {
 		return this.bookService.findBookByName(name);
 	}
 	
-	@GetMapping("/book/{price}")
+	@GetMapping(path="/book/{price}")
 	public Book findBookByPrice(@PathVariable int price) {
 		return this.bookService.findBookByPrice(price);
 	}
-	@PutMapping("/book/{id}")
+	@PutMapping(path="/book/{id}")
 	public Book updateBookById(@PathVariable int id,@RequestBody Book book) {
 		return this.bookService.updateBookById(id, book);
 	}
-	@DeleteMapping("/book/{id}")
+	@DeleteMapping(path="/book/{id}")
 	public void deleteBookById(@PathVariable int id) {
 		 this.bookService.deleteBookById(id);
 		 
